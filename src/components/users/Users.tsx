56b51e78a6df8fc8b0354904956f798fe7715c4e
Bishwajit Jha <ajitjha393@gmail.com>
2020-06-08 16:44:46 +0530

    Refactored Users to FC

--- src/components/users/Users.tsx
+++ src/components/users/Users.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { FC } from 'react'
 import UserItem from './UserItem'
 import { UserItemType } from '../../../types/index'
 
@@ -7,23 +7,20 @@ interface UserProps {
 	users: UserItemType[]
 }
 
-class Users extends Component<UserProps> {
-	render() {
-		const { users } = this.props
-
-		const userStyle = {
-			display: 'grid',
-			gridTemplateColumns: 'repeat(3,1fr)',
-			gridGap: '1rem',
-		}
-		return (
-			<div style={userStyle}>
-				{users.map((user) => (
-					<UserItem key={user.id} user={user} />
-				))}
-			</div>
-		)
+const Users: FC<UserProps> = ({ users, loading }) => {
+	const userStyle = {
+		display: 'grid',
+		gridTemplateColumns: 'repeat(3,1fr)',
+		gridGap: '1rem',
 	}
+
+	return (
+		<div style={userStyle}>
+			{users.map((user) => (
+				<UserItem key={user.id} user={user} />
+			))}
+		</div>
+	)
 }
 
 export default Users
