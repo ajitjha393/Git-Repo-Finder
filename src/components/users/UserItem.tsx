6aaaead606d8972c0b0309f1c99d7e639c1acaa6
Bishwajit Jha <ajitjha393@gmail.com>
2020-06-09 00:24:49 +0530

    Changed a to Link

--- src/components/users/UserItem.tsx
+++ src/components/users/UserItem.tsx
@@ -1,5 +1,6 @@
 import React, { FC } from 'react'
 import { UserItemType } from '../../../types/index'
+import { Link } from 'react-router-dom'
 
 interface UserItemProps {
 	user: UserItemType
@@ -18,9 +19,12 @@ const UserItem: FC<UserItemProps> = ({
 			/>
 			<h3>{login}</h3>
 			<div>
-				<a href={html_url} className="btn btn-dark btn-sm my-1">
+				<Link
+					to={`/user/${login}`}
+					className="btn btn-dark btn-sm my-1"
+				>
 					More
-				</a>
+				</Link>
 			</div>
 		</div>
 	)
