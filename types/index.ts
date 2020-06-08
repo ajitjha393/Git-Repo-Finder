bc18f4af0f5a4c477c121884c8aca8dbb8256d38
Bishwajit Jha <ajitjha393@gmail.com>
2020-06-09 00:24:01 +0530

    Added A single User Type Fetched from github Api

--- types/index.ts
+++ types/index.ts
@@ -4,3 +4,18 @@ export interface UserItemType {
 	avatar_url: string
 	html_url: string
 }
+
+export interface UserType {
+	name: string
+	avatar_url: string
+	location: string
+	bio: string
+	blog: string
+	login: string
+	html_url: string
+	followers: number
+	following: number
+	public_repos: number
+	public_gists: number
+	hireable: boolean
+}
